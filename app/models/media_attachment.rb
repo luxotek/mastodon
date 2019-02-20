682a4c01afd2ffa497426cb9387039986d7c5df6
Luxotek <luxotek@gmail.com>
2019-02-19 18:18:03 -0800

    Change video output style of audio files

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -51,7 +51,7 @@ class MediaAttachment < ApplicationRecord
       format: 'mp4',
       convert_options: {
         output: {
-          filter_complex: '"[0:a]compand,showwavespic=s=640x360:split_channels=1,format=yuv420p[v]"',
+          filter_complex: '"[0:a]compand,showwaves=s=640x360:split_channels=1:mode=cline:colors=blue|blue,format=yuv420p[v]"',
           map: '"[v]" -map 0:a', 
           threads: 2,
           vcodec: 'libx264',
